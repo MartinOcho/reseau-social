6901b45dcee5f4840551cbad71273d8de904c5d5
OchoKOM <martinkababo@gmail.com>
2026-03-11 19:52:18 +0100

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,48 @@
-﻿export default function RootLayout({ children }: { children: React.ReactNode }) {
+﻿import { Metadata } from "next";
+import ReactQueryProvider from "./ReactQueryProvider";
+import localFont from "next/font/local";
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | OchoApp",
+    default: "OchoApp"
+  },
+  description: "The social media app for power nerd",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ReactQueryProvider>
+          <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
+        <Toaster/>
+      </body>
     </html>
   );
 }
